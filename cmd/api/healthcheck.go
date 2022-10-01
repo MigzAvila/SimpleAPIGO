cf5bdbfde6e1c90f0954377c2be3b58e607300cd
Miguel Avila <98852302+MiguelAAvila@users.noreply.github.com>
2022-10-01 11:41:43 -0600

    reformat healthcheck

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -9,9 +9,11 @@ import (
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	// Create a map to
 	data := envelope{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+		"status": "available",
+		"system_info": envelope{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
