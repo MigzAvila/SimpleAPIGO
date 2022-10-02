20e27fdec7568beb905f51545a6aa5756f9b0c21
Miguel Avila <98852302+MiguelAAvila@users.noreply.github.com>
2022-10-01 21:47:14 -0600

    added utility function for validation

---
+++ internal/validator/validator.go
@@ -0,0 +1,79 @@
+// Filename : internal/validator/validator.go
+
+package validator
+
+import (
+	"net/url"
+	"regexp"
+)
+
+var (
+	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+	PhoneRX = regexp.MustCompile(`^\+?\(?[0-9]{3}\)?\s?-\s?[0-9]{3}\s?-\s?[0-9]{4}$`)
+)
+
+// create a type that wraps the validation errors map
+
+type Validator struct {
+	Errors map[string]string
+}
+
+// Create a new instance of Validator
+func New() *Validator {
+	return &Validator{
+		Errors: make(map[string]string),
+	}
+}
+
+// Valid checks the Errors map for entries
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+// In() checks if elements can be found in a provided list of elements
+func In(elements string, list ...string) bool {
+	for i := range elements {
+
+		if elements == list[i] {
+			return true
+		}
+
+	}
+	return false
+}
+
+// Matches() returns true if the provided string matches a specific regexp pattern
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
+// ValidWebsite checks if the provided string is a valid website URL
+func ValidWebsite(website string) bool {
+	_, err := url.Parse(website)
+	return err == nil
+}
+
+// AddError() adds an error entry to the Error map
+func (v *Validator) AddError(key, message string) {
+	if _, exists := v.Errors[key]; exists {
+		v.Errors[key] = message
+	}
+}
+
+// Check() preforms the validation checks and calls the AddError method in turn if there is an error
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
+
+// Unique() checks that there are no repeating values in the slice
+func Unique(values []string) bool {
+	uniqueValues := make(map[string]bool)
+	for _, value := range values {
+		uniqueValues[value] = true
+	}
+
+	return len(uniqueValues) == len(values)
+}
